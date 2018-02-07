9025e2a5ad6e545eb09980b4667aa80cb151d03f
Brian <36174148+brianrasmu@users.noreply.github.com>
2018-02-06 22:09:26 -0300

    Actualización

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,21 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad=document.getElementById('edad').value;
+	if(!(edad>13 && edad<17))
+	{
+		alert("No es adolescente.");
+	}
+
+/*
+	if(edad>12 && edad<17)
+	{
+	}
+	else
+	{
+		alert("No es adolescente.");
+	}
+*/
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,33 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad=document.getElementById('edad').value;
+	if (edad<13) 
+	{
+		alert("Niño.");
+	}
+	else
+		if (edad>17) 
+		{
+		alert("Mayor.");
+		}
+		else
+			alert("Adolescente.");
 
-
+/*
+	if (edad>17) 
+	{
+		alert("Mayor.");
+	}
+	if(edad>12 && edad<18) 
+	{
+		alert("Adolescente.");
+	}
+	if (edad<13) 
+	{
+		alert("Niño.");
+	}
+*/
 
 }//FIN DE LA FUNCIÓN
